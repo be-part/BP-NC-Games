cc6117351fcb19cb579b519aa8f33cd113f26249
catsAndIvy <bronwyn-elizabeth@live.co.uk>
2023-05-10 11:01:52 +0100

    remove T4 from main branch

--- db/controllers/reviews.controllers.js
+++
@@ -1,7 +0,0 @@
-const { fetchReviews } = require("../models/reviews.models")
-
-exports.getReviews = (request, response) => {
-    fetchReviews(request.params.review_id).then((reviews)=>{
-        response.status(200).send({ reviews: reviews }) 
-      })
-}
